0c183384fef29c4409867993914a9d48a270cc0c
Saurabh Singh <62989453+saurabhsingh20@users.noreply.github.com>
2021-05-08 19:58:23 +0530

    Add files via upload

---
+++ FloydsCycleAlgorithm/Main.java
@@ -0,0 +1,32 @@
+package com.Saurabh;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+
+    public class Main {
+        public ListNode detectCycle(ListNode head) {
+            if (head == null || head.next == null) {
+                return null;
+            }
+            ListNode slow = head, fast = head;
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+                if (fast == slow) {
+                    while (head != fast) {
+                        fast = fast.next;
+                        head = head.next;
+                    }
+                    return head;
+                }
+            }
+            return null;
+        }
+    }
+}
